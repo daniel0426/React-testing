4da1a0f5a1d2c5ced79cae96ce753378efd312f2
daniel0426 <angus9209@gmail.com>
2022-03-23 10:15:06 +1300

    habitAddForm component snapshop test complete

--- src/components/form/tests/habitAddForm.test.js
+++ src/components/form/tests/habitAddForm.test.js
@@ -3,36 +3,44 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import HabitAddForm from '../habitAddForm';
+import renderer from 'react-test-renderer';
 
 describe('HabitAddForm', ()=> {
-    let onAdd;
-    let input;
-    let button;
 
-    beforeEach(()=> {
-        onAdd = jest.fn();
-
-        //Arrange
-        render(<HabitAddForm onAdd={onAdd} />);
-        input =  screen.getByPlaceholderText('Habit');
-        button= screen.getByText('Add');
+    it('renders', ()=> {
+        //snapshot test 
+        const component = renderer.create(<HabitAddForm onAdd={jest.fn()}/>)
+        expect(component.toJSON()).toMatchSnapshot();
     })
 
-    it('calls onAdd when button is clicked and valid habit is entered', ()=> {
-        //Act
-        userEvent.type(input, 'New Habit');
-        userEvent.click(button);
-
-        //Assert
-        expect(onAdd).toHaveBeenCalledWith('New Habit');
+    describe('Form Submit', ()=> {
+        let onAdd;
+        let input;
+        let button;
+    
+        beforeEach(()=> {
+            onAdd = jest.fn();
+            //Arrange
+            render(<HabitAddForm onAdd={onAdd} />);
+            input =  screen.getByPlaceholderText('Habit');
+            button= screen.getByText('Add Habit');
+        })
+    
+        it('calls onAdd when button is clicked and valid habit is entered', ()=> {
+            //Act
+            userEvent.type(input, 'New Habit');
+            userEvent.click(button);
+            //Assert
+            expect(onAdd).toHaveBeenCalledWith('New Habit');
+        })
+    
+        it('does not call onAdd when the habit is empty', ()=> {
+             //Act
+             userEvent.type(input, '');
+             userEvent.click(button);
+             //Assert
+             expect(onAdd).toHaveBeenCalledTimes(0);
+        })
     })
-
-    it('does not call onAdd when the habit is empty', ()=> {
-         //Act
-         userEvent.type(input, '');
-         userEvent.click(button);
- 
-         //Assert
-         expect(onAdd).toHaveBeenCalledTimes(0);
-    })
+    
 })
